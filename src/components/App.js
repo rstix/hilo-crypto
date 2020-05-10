3cb1f9264bde144276878bb510dcb4be30e241b1
Roman Stix <roman.stix@digitalsunray.com>
2020-05-10 11:21:40 +0200

    intial commit

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <button className="btn">Get Started</button>
+    </div>
+  );
+}
+
+export default App;
